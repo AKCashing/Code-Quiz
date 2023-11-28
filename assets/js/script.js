1cf23c15aff2b4fa12362458fe7acc0292786741
cpayment <cpayment12@gmail.com>
2023-11-27 18:37:33 -0600

    Added index.html, style.css, and style.js. Created a main screen and first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 var startButtonEl = document.getElementById('start-button');
+var startScreenEl = document.getElementById('start-screen');
 
-function startGame() {
-    console.log('Click');
-}
+var question1El = document.getElementById('question1');
+var q1Answer1El = document.getElementById('question1-button1');
+var q1Answer2El = document.getElementById('question1-button2');
+var q1Answer3El = document.getElementById('question1-button3');
+var q1Answer4El = document.getElementById('question1-button4');
 
 startButtonEl.addEventListener('click', startGame);
+
+question1El.style.display = 'none';
+
+function startGame() {
+    startScreenEl.style.display = 'none';
+    question1El.style.display = 'flex'
+}
